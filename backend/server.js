6fc9ec03bc65b718ff70887e0892b6ef3f90319f
lokeshwarreddychallapu <2200030516@kluniversity.in>
2026-05-02 16:54:37 +0530

    remove frontend build serving

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/users', require('./routes/users'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+    res.send("Backend is LIVE 🚀");
   });
 }
 

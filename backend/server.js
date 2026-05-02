f4984a7ddadd352293048ef609c9dfa584307c7e
lokeshwarreddychallapu <2200030516@kluniversity.in>
2026-05-02 16:27:33 +0530

    Fix Mongo URI and port

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,16 @@ if (process.env.NODE_ENV === 'production') {
 
 // Connect DB and start server
 const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
-  .catch((err) => console.error('DB connection error:', err));
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
